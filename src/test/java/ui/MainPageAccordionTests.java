aeb2f24c87ae2c1b8ccac068d32c6c17ec691ab8
klirovskij <135542945+klirovskij@users.noreply.github.com>
2023-10-10 14:55:04 +0300

    Update MainPageAccordionTests.java

--- src/test/java/ui/MainPageAccordionTests.java
+++ src/test/java/ui/MainPageAccordionTests.java
@@ -1,82 +1,38 @@
 package ui;
 
-import org.junit.After;
-import org.junit.Before;
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
+import pageObjects.MainPage;
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.fail;
 
-public class MainPageAccordionTests {
-    private WebDriver driver; // Поле для хранения экземпляра WebDriver
-
-    /** Веб-драйвер */
-    private final String mainPageUrl = "https://qa-scooter.praktikum-services.ru";
-
-    /** Порядковый номер элемента аккордеона */
-    private final int numberOfElement;
-
-    /** Ожидаемый текст в заголовке элемента аккордеона */
-    private final String expectedHeaderText;
-
-    /** Ожидаемый текст в раскрывающемся блоке элемента аккордеона */
-    private final String expectedItemText;
-
-    /**
-     * Конструктор класса MainPageAccordionTests
-     * @param numberOfAccordionItem Порядковый номер элемента аккордеона
-     * @param expectedHeaderText Ожидаемый текст в заголовке элемента аккордеона
-     * @param expectedItemText Ожидаемый текст в раскрывающемся блоке элемента аккордеона
-     */
-    public MainPageAccordionTests(int numberOfAccordionItem, String expectedHeaderText, String expectedItemText) {
-        this.numberOfElement = numberOfAccordionItem;
-        this.expectedHeaderText = expectedHeaderText;
-        this.expectedItemText = expectedItemText;
-    }
-
-    @Before
-    public void setup() {
-        // Установка пути к драйверу Chrome
-        System.setProperty("webdriver.chrome.driver", "/Users/hoodozhnik/Desktop/Практикум \"Автоматизация тестирования\"/chromedriver_mac_arm64"); // Замените на реальный путь к драйверу
-
-        // Создание экземпляра WebDriver
-        driver = new ChromeDriver();
-
-        driver.get(mainPageUrl); // Открытие главной страницы
-    }
-
+public class MainPageAccordionTests extends BaseTest {
     @Test
-    public void checkAccordionIsCorrect() {
-        // Ваш тестовый код для проверки аккордеона
-        WebElement accordionHeader = driver.findElement(By.xpath("//xpath-вашего-элемента-заголовка"));
-        accordionHeader.click();
+    public void testAccordionItems() {
+        MainPage mainPage = new MainPage(this.webDriver);
 
-        WebElement accordionItem = driver.findElement(By.xpath("//xpath-вашего-элемента-раскрывающегося-блока"));
+        mainPage.clickOnCookieAcceptButton();
 
-        if (accordionItem.isDisplayed()) {
-            assertThat("Problems with text in accordion header #" + this.numberOfElement,
-                    this.expectedHeaderText,
-                    equalTo(accordionHeader.getText())
+        // Проверка основной страницы аренды самоката
+        int numberOfAccordionItems = 8;
+
+        for (int i = 0; i < numberOfAccordionItems; i++) {
+            mainPage.clickAccordionHeader(i);
+            mainPage.waitForLoadItem(i);
+
+            String headerText = mainPage.getAccordionHeaderText(i);
+            String itemText = mainPage.getAccordionItemText(i);
+
+            MatcherAssert.assertThat(
+                    "Problem with accordion item #" + i + " header text",
+                    headerText,
+                    equalTo("Expected Header Text " + i)
             );
-            assertThat("Problems with text in accordion item #" + this.numberOfElement,
-                    this.expectedItemText,
-                    equalTo(accordionItem.getText())
-            );
-        } else {
-            fail("Accordion header item #" + this.numberOfElement + " didn't load");
-        }
-    }
 
-    @After
-    public void teardown() {
-        // Закрытие браузера
-        if (driver != null) {
-            driver.quit();
+            MatcherAssert.assertThat(
+                    "Problem with accordion item #" + i + " item text",
+                    itemText,
+                    equalTo("Expected Item Text " + i)
+            );
         }
     }
 }
